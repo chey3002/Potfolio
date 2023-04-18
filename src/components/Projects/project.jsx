bef332293f437318d3e665b9f2af830207b8e5a7
SrChey <carlos-avb@hotmail.com>
2023-04-17 23:25:50 -0500

    corrección de estilos de proyectos

--- src/components/Projects/project.jsx
+++ src/components/Projects/project.jsx
@@ -8,23 +8,22 @@ export default function Project({ myProject }) {
   })
   console.log(mySkills);
   return (
-    <a href={myProject.url}>
+    <a className='max-w-md flex justify-center align-middle' href={myProject.url}>
     <article className=' justify-center items-center border-2 border-neonPink2 rounded-lg p-4 m-4'>
-      
       <header>
           <h2 className="text-neonPink drop-shadow-neonPinkGlow  w-fit sm:text-xl sm:leading-relaxed">
             {myProject.name}
           </h2>
           </header>
         <hr className='sm:w-11/12 mx-auto mt-5 text-neonPink drop-shadow-neonPinkGlow' />
-        <div className='inline-block overflow-hidden m-2' ><img className=' block transition-all duration-500 hover:scale-125 hover:contrast-125  max-h-96 ' src={myProject.img}></img></div>
-        <p className='h-20 overflow-auto'>{myProject.description}</p>
+        <div className='inline-block overflow-hidden m-2 ' ><img className=' block transition-all duration-500 hover:scale-125 hover:contrast-125  max-h-80 ' src={myProject.img} width='auto' height='320px'></img></div>
+        <p className='lg:h-24 overflow-auto'>{myProject.description}</p>
                 <hr className='sm:w-11/12 mx-auto mt-5 text-neonPink drop-shadow-neonPinkGlow' />
 
         <div>
           <h3 className="text-neonPink2 drop-shadow-neonPinkGlow  w-fit sm:text-xl sm:leading-relaxed">
                     Technologies:
-                </h3>
+          </h3>
           {mySkills.map((skill,i) => {
                     return <Tag key={i} skill={skill} />;
                 })}
--- src/components/Projects/projects.jsx
+++ src/components/Projects/projects.jsx
@@ -3,13 +3,13 @@ import {projects} from '../../utils/proyectos'
 import Project from './project'
 export default function Projects() {
   return (
-      <section className=" w-full sm:h-screen sm:grid sm:place-content-center " id="Projects">
+      <section className=" w-full sm:h-screen flex justify-evenly flex-col " id="Projects">
           <header className="w-fit mx-auto text-neonPink2 drop-shadow-neonPinkGlow  sm:text-2xl sm:leading-relaxed">
               <h1>
                     Projects
                 </h1>
         </header>
-          <div className='grid place-content-center grid-cols-[repeat(auto-fit,minmax(300px,1fr))] gap-2 w-full'>
+          <div className='flex flex-wrap justify-evenly'>
               {projects.map((project, i) => <Project key={i} myProject={project}></Project>)}
           </div>
     </section>
